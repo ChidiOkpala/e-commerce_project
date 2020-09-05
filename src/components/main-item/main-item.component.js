a90860042350e64e4e47af4353084b4792664cc6
Chidi Okpala <63919087+ChidiOkpala@users.noreply.github.com>
2020-09-05 17:47:19 +0100

    building of the /shop page and routing of the pages

--- src/components/main-item/main-item.component.js
+++ src/components/main-item/main-item.component.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { withRouter } from 'react-router-dom';
+
 import './main-item.styles.scss';
 
-const MainItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MainItem = ({title, imageUrl, size, history, match, linkUrl}) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className="content">
             <h1 classname="title">{title.toUpperCase()}</h1>
@@ -12,4 +14,4 @@ const MainItem = ({title, imageUrl, size}) => (
     </div>
 )
 
-export default MainItem;
+export default withRouter(MainItem);
