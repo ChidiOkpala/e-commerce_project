a6727a19062d537e20128ab0c33ec508d456602b
Chidi Okpala <63919087+ChidiOkpala@users.noreply.github.com>
2020-09-06 01:16:36 +0100

    implementing of signin

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component' 
+import ShopPage from './pages/shop/shop.component';
+import Header from '../src/components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
     <div>
+        <Header />
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/sign-in' component={SignInAndSignUpPage}/>
     </div>
   );
 }
