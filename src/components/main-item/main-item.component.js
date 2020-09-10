b07a338d49eecef76a94953d0a3793377e5ab3e0
Chidi Okpala <63919087+ChidiOkpala@users.noreply.github.com>
2020-09-10 15:23:34 +0100

    added redux to the project and mjapped state and dispatch to props in header and app.js respectively

--- src/components/main-item/main-item.component.js
+++ src/components/main-item/main-item.component.js
@@ -8,7 +8,7 @@ const MainItem = ({title, imageUrl, size, history, match, linkUrl}) => (
     <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className="content">
-            <h1 classname="title">{title.toUpperCase()}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -32,7 +32,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
                 ...additionalData
             })
         } catch(error){
-            console.log('error createing user', error.message);
+            console.log('error creating user', error.message);
         }
     };
     return userRef;
@@ -44,7 +44,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ Prompt: 'select_account' });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
